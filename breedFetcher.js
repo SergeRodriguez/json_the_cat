d90fc5d443b5ae698032a4713b802b3c5a0d9708
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-10-24 20:42:12 +0000

    refactored breedFetcher to take a string and a callback as arguments and called the function in index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,22 @@
 const request = require("request");
-const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + process.argv[2];
 
-request(URL, (error, response, content) => {
 
-  if (error) {
-    console.log("URL is wrong");
-  } else {
-    const data = JSON.parse(content);
-    if (!data[0]) {
-      console.log("The requested breed is not found.");
+const fetchBreedDescription = function(argument1, _callback) {
+  const URL = "https://api.thecatapi.com/v1/breeds/search?q=" + argument1;
+  request(URL, (error, response, content) => {
+    if (error) {
+      _callback("URL is wrong");
     } else {
-      console.log(data[0].description);
+      const data = JSON.parse(content);
+      if (!data[0]) {
+        _callback("The requested breed is not found.");
+      } else {
+        _callback(error, data[0].description.trim());
+      }
     }
-  }
+  });
+};
 
-});
+
+
+module.exports = { fetchBreedDescription };
